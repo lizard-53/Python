5690e652b0371600312d926de699917b7de18492
Lizard53 <glynn.jacob@gmail.com>
2026-07-29 15:06:16 +0100

    basic regex json library & modified script to extract email addresses from an input

--- Test.py
+++ Test.py
@@ -1,1 +1,13 @@
-print ("Test upload to Github")
+# Test upload to Github
+
+import json
+import re
+
+with open("regex.json", "r") as file:
+    regex_bank = json.load(file)
+
+email_regex = regex_bank["email_extract_pattern"]
+
+user_input = "us_er.he5638llo-test@gmail.com"
+if re.match(email_regex, user_input):
+    print("email found")
