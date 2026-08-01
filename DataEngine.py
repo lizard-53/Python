bd4620adc36477b21761a7e2927f70893f4b2523
Lizard53 <glynn.jacob@gmail.com>
2026-08-01 09:43:07 +0100

    id() function updated, can now find a number and email on the same line. Need to update it further with re.findall() instead of re.search() so that multiple cases of a search parameter on the same line get picked up. Currently, will only harvest the first email and phone number found on a line.

--- DataEngine.py
+++ DataEngine.py
@@ -2,40 +2,31 @@ import re
 import json
 
 class DataEngine:
-	def __init__(self, file):
-		self.file = file
+	def __init__(self, data_file):
+		self.file = data_file
 		
-		with open ("regex.json","r") as file:
-				self.regex = json.load(file)
+		with open ("regex.json","r") as json_file:
+				self.regex = json.load(json_file)
 
 		self.temp_storage = []
 		self.email_storage = []
 		self.phone_storage = []
 		
 	def scan(self):
-		with open (self.file,"r") as file:
-			for line in file:
+		with open (self.file,"r") as data_file:
+			for line in data_file:
 				self.temp_storage.append((line.strip()))
-
-	# the id function is correctly capturing lines where the regex patterns are matched.
-	# the id function is failing to split that line and only return the correct pattern.
-	# Instead, the entire line is added to the storage list and erroneous data is displayed 
-	# in the extracted list.
-
 
 	def id(self):
 		for line in self.temp_storage:
 			email_match = re.search(self.regex["email_extract_pattern"],line)
 			phone_match = re.search(self.regex["uk_phone_extract_pattern"],line)
+
 			if email_match:
-				self.email_storage.append(line)
-			elif phone_match:
-				self.phone_storage.append(line)
-			else:
-				print (f"Value Invalid: {line}")
-
-		
-
+				self.email_storage.append(email_match.group())
+			if phone_match:
+				self.phone_storage.append(phone_match.group())
+			
 	def extract(self):
 		print("\nExtracted list:")		
 		for line in self.email_storage:
@@ -44,16 +35,10 @@ class DataEngine:
 		for line in self.phone_storage:
 			print(line)
 		
-
 	def display(self):
 		pass
-
 
 p1 = DataEngine("data.txt")
 p1.scan()
 p1.id()
 p1.extract()
-
-# Test: printing one of my regex commands from the json file
-#print(p1.regex["email_extract_pattern"])
-
