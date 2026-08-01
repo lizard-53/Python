ce19777943f39ff3d8f9f843432899f9d1c28073
Lizard53 <glynn.jacob@gmail.com>
2026-08-01 10:24:10 +0100

    Redundant functions removed, I think only 1 function needed.
    The program was not efficient, storing
    the entire file in a list before operating on
    the lines would have caused issues for large files.
    
    Replaced re.search() with re.findall()
    
    Resolves previous issue where only 1 match is collected
    for a given pattern on each line.
    Now, multiple emails or phone numbers on the same
    line are collected.

--- DataEngine.py
+++ DataEngine.py
@@ -8,37 +8,26 @@ class DataEngine:
 		with open ("regex.json","r") as json_file:
 				self.regex = json.load(json_file)
 
-		self.temp_storage = []
 		self.email_storage = []
 		self.phone_storage = []
-		
-	def scan(self):
+
+
+	def parse_file(self):
 		with open (self.file,"r") as data_file:
 			for line in data_file:
-				self.temp_storage.append((line.strip()))
+				email_match = re.findall(self.regex["email_extract_pattern"],line)
+				phone_match = re.findall(self.regex["uk_phone_extract_pattern"],line)
 
-	def id(self):
-		for line in self.temp_storage:
-			email_match = re.search(self.regex["email_extract_pattern"],line)
-			phone_match = re.search(self.regex["uk_phone_extract_pattern"],line)
+				if email_match:
+					self.email_storage.extend(email_match)
+				if phone_match:
+					self.phone_storage.extend(phone_match)
 
-			if email_match:
-				self.email_storage.append(email_match.group())
-			if phone_match:
-				self.phone_storage.append(phone_match.group())
-			
-	def extract(self):
 		print("\nExtracted list:")		
 		for line in self.email_storage:
-			print(line)
-		
+			print(line)	
 		for line in self.phone_storage:
 			print(line)
-		
-	def display(self):
-		pass
 
 p1 = DataEngine("data.txt")
-p1.scan()
-p1.id()
-p1.extract()
+p1.parse_file()
