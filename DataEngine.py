df01027abe5fca46613e760293c200d2cd100fa5
Lizard53 <glynn.jacob@gmail.com>
2026-08-02 17:39:24 +0100

    Force apply gitignore rules

--- DataEngine.py
+++ DataEngine.py
@@ -1,5 +1,8 @@
 import re
 import json
+import sys
+import pandas as pd
+
 
 class DataEngine:
 	def __init__(self, data_file):
@@ -7,7 +10,7 @@ class DataEngine:
 		
 		with open ("regex.json","r") as json_file:
 				self.regex = json.load(json_file)
-
+				
 		self.email_storage = []
 		self.phone_storage = []
 
@@ -23,11 +26,36 @@ class DataEngine:
 				if phone_match:
 					self.phone_storage.extend(phone_match)
 
-		print("\nExtracted list:")		
-		for line in self.email_storage:
-			print(line)	
-		for line in self.phone_storage:
-			print(line)
+		email_col = pd.Series(self.email_storage, name="Email Addresses")
+		phone_col = pd.Series(self.phone_storage, name="UK Phone Numbers")
+		df = pd.concat([email_col, phone_col], axis=1)
 
-p1 = DataEngine("data.txt")
-p1.parse_file()
+		output_file_path = "extracted_data_list.xlsx"
+
+		with pd.ExcelWriter(
+			output_file_path,
+			engine="xlsxwriter",
+			engine_kwargs={"options": {"strings_to_numbers": False}},
+		) as writer:
+			df.to_excel(writer, index=False, sheet_name="Contacts")
+
+		print("\nData successfully saved to extracted_data_list.xlsx")
+
+
+def main():
+	if len(sys.argv)==2:
+		data_file = sys.argv[1]
+	else:
+		data_file = (input(f"Enter file name: "))
+
+	try:
+		p1 = DataEngine(data_file)
+		p1.parse_file()
+	except FileNotFoundError:
+		print(f"Error: The file {data_file} could not be found.")
+		main()
+	except Exception as e:
+		print(f"An unexpected error occured: {e}")
+
+if __name__ == "__main__":
+	main()
